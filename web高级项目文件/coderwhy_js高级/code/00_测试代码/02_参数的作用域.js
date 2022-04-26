16113e12cc1a47918084c927d65216614a4a418f
min-47 <1943227287@qq.com>
2022-04-26 11:13:58 +0800

    coderwhy的js高级教程源码相关

---
+++ web高级项目文件/coderwhy_js高级/code/00_测试代码/02_参数的作用域.js
@@ -0,0 +1,18 @@
+var x = 0
+
+// 当函数的参数有默认值时, 会形成一个新的作用域, 这个作用域用于保存参数的值
+function foo(x, y = function() {x=3, console.log(x) }) {
+  console.log(x)
+  var x = 2
+  // console.log(x)
+  y()
+  console.log(x)
+}
+
+foo()
+console.log(x)
+
+// undefined
+// 3
+// 2
+// 0
